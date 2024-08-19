2694920f646f7d4e0846fcf09dc6996f7c73153b
Kolokolcev Kirill <kirill.kolokolcev.motmom@gmail.com>
2024-08-19 09:20:06 +0300

    Remove comments

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,28 +1,6 @@
 import * as TaskFormulations from './TaskFormulation';
 import * as TaskImplementations from './TaskImplementation';
 
-// import FirstTaskFormulation from './TaskFormulation/First.vue';
-// import SecondTaskFormulation from './TaskFormulation/Second.vue';
-// import ThirdTaskFormulation from './TaskFormulation/Third.vue';
-// import FourthTaskFormulation from './TaskFormulation/Fourth.vue';
-// import FifthTaskFormulation from './TaskFormulation/Fifth.vue';
-
-// import FirstTaskImplementation from './TaskImplementation/First.vue';
-// import SecondTaskImplementation from './TaskImplementation/Second.vue';
-// import ThirdTaskImplementation from './TaskImplementation/Third.vue';
-// import FourthTaskImplementation from './TaskImplementation/Fourth.vue';
-// import FifthTaskImplementation from './TaskImplementation/Fifth.vue';
-
 import TaskLayout from './TaskLayout.vue';
 
 export { TaskLayout, TaskFormulations, TaskImplementations };
-// export { TaskLayout, FirstTaskFormulation,
-//   SecondTaskFormulation,
-//   ThirdTaskFormulation,
-//   FourthTaskFormulation,
-//   FifthTaskFormulation,
-//   FirstTaskImplementation,
-//   SecondTaskImplementation,
-//   ThirdTaskImplementation,
-//   FourthTaskImplementation,
-//   FifthTaskImplementation, };
